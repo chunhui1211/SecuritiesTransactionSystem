d6aa1c1ef286b82ac6a9ae6e63930edb0359e5e5
Alison <chunhui831211@gmail.com>
2026-02-27 14:56:03 +0800

    feat:update swagger description

--- SecuritiesTransactionSystem.Backend/Program.cs
+++ SecuritiesTransactionSystem.Backend/Program.cs
@@ -1,5 +1,10 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using SecuritiesTransactionSystem.Repository;
+using SecuritiesTransactionSystem.Service;
+using SecuritiesTransactionSystem.Service.Interface;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,9 +13,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "證券交易系統 API",
+        Description = "這是一個使用 .NET 8 實作的股票查詢與下單系統範例。",
+    });
+
+    // 取得 XML 檔案路徑
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 builder.Services.AddDbContext<TradingDbContext>(opt => opt.UseInMemoryDatabase("TradingDb"));
+
+builder.Services.AddHttpClient<IStockService, StockService>();
 
 var app = builder.Build();
 
