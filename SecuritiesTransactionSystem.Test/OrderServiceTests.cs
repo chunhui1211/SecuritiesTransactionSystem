e9267bfc54a3ae20fc67986b35dcf2f6cb91381e
Alison <chunhui831211@gmail.com>
2026-02-28 14:14:59 +0800

    feat:add OrderService unit test
    單元測試

---
+++ SecuritiesTransactionSystem.Test/OrderServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using SecuritiesTransactionSystem.Entity.DTOs;
+using SecuritiesTransactionSystem.Entity.Enums;
+using SecuritiesTransactionSystem.Entity.Exceptions;
+using SecuritiesTransactionSystem.Repository.Interface;
+using SecuritiesTransactionSystem.Service;
+
+namespace SecuritiesTransactionSystem.Test
+{
+    public class OrderServiceTests
+    {
+        private readonly Mock<IOrderRepository> _mockRepo;
+        private readonly OrderService _orderService;
+
+        public OrderServiceTests()
+        {
+            _mockRepo = new Mock<IOrderRepository>();
+            _orderService = new OrderService(_mockRepo.Object, new NullLogger<OrderService>());
+        }
+
+        [Fact]
+        public async Task CreateOrder_SellWithNoStock_ShouldThrowBusinessException()
+        {
+            var sellRequest = new CreateOrderRequest
+            {
+                Symbol = "2330",
+                Side = SideType.Sell,
+                Quantity = 10,
+                Price = 600
+            };
+
+            var exception = await Assert.ThrowsAsync<BusinessException>(() =>
+                _orderService.CreateOrderAsync(sellRequest));
+
+            Assert.Contains("持股餘額不足", exception.Message);
+        }
+    }
+}
